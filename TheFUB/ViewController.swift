f582d17f3800f9dde200146aec996ffb476423b8
gloomya <gloomya@gmail.com>
2020-03-02 17:59:58 -0500

    Initial commit

--- TheFUB/ViewController.swift
+++ TheFUB/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var appicon: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 3) {
+            self.appicon.alpha = 1
+            
+        }
+        loadmain()
     }
-
-
+    
+    func loadmain () {
+    let when = DispatchTime.now() + 3
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            self.performSegue(withIdentifier: "mainpage", sender: nil)
+        }
+    
+    }
 }
-
